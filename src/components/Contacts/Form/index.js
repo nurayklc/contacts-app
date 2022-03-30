cc677d1f0f44d0e4a5d513730e5fed98f85e50a2
nurayklc <nuraykilic0101@gmail.com>
2022-03-30 17:39:19 +0300

    Complete form component

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-function Form() {
+const initialFormValues = { fullname: '', phoneNumber: '' }
+
+function Form({ addContacts, contacts }) {
+  const [form, setform] = useState(initialFormValues);
+
+  useEffect(() => {
+    setform(initialFormValues)
+  }, [contacts])
+  
+
+  const onChangeInput = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    // formun oto sayfa yenileme işlemini engellemek için yapılır
+    e.preventDefault();
+
+    if (form.fullname === '' || form.phoneNumber === '') {
+      return false;
+    }
+    addContacts([...contacts, form]);
+  };
+
   return (
-    <div>Form</div>
-  )
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          value={form.fullname}
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <input
+          name="phoneNumber"
+          placeholder="Phone Number"
+          value={form.phoneNumber}
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <button>Add</button>
+      </div>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
