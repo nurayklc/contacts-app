2c75527604e8cb59208fdce8a2c902933bd84ffd
nurayklc <nuraykilic0101@gmail.com>
2022-04-04 17:58:24 +0300

    Complete list filter

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import { useState } from "react";
 
-function List({contacts }) {
+function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase())
+    );
+  });
+
   return (
     <div>
-        <ul>
-            {contacts.map((contact,i) => (
-                <li key={i}>{contact.fullname}</li>
-            ))}
-        </ul>
+      <input
+        placeholder="Filter contact"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
+      <ul>
+        {filtered.map((contact, i) => (
+          <li key={i}>{contact.fullname}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,19 +1,23 @@
-import { useState, useEffect} from 'react'
-import List from './List'
-import Form from './Form'
+import { useState, useEffect } from "react";
+import List from "./List";
+import Form from "./Form";
 
 function Contacts() {
-    const [contacts, setContacts] = useState([])
-    useEffect(() => {
-        console.log(contacts)
-    }, [contacts])
-    
+  const [contacts, setContacts] = useState([
+    { fullname: " Mehmet", phoneNumber: "123123" },
+    { fullname: " Ali", phoneNumber: "456456" },
+    { fullname: " Fatma", phoneNumber: "789789" },
+  ]);
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
   return (
     <div>
-        <List contacts={contacts} />
-        <Form contacts={contacts} addContacts={setContacts}/>
+      <List contacts={contacts} />
+      <Form contacts={contacts} addContacts={setContacts} />
     </div>
-  )
+  );
 }
 
-export default Contacts
+export default Contacts;
